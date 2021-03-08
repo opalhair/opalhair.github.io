dcb4bf50be74082f4cf4876223d32fd6cf64d473
opalhair <opalhair@gmail.com>
2021-03-08 16:58:57 +0800

    Site updated: 2021-03-08 16:58:56

--- 404/404.js
+++ 404/404.js
@@ -1,87 +1,19 @@
-gsap.set("svg", { visibility: "visible" });
-gsap.to("#headStripe", {
-  y: 0.5,
-  rotation: 1,
-  yoyo: true,
-  repeat: -1,
-  ease: "sine.inOut",
-  duration: 1
-});
-gsap.to("#spaceman", {
-  y: 0.5,
-  rotation: 1,
-  yoyo: true,
-  repeat: -1,
-  ease: "sine.inOut",
-  duration: 1
-});
-gsap.to("#craterSmall", {
-  x: -3,
-  yoyo: true,
-  repeat: -1,
-  duration: 1,
-  ease: "sine.inOut"
-});
-gsap.to("#craterBig", {
-  x: 3,
-  yoyo: true,
-  repeat: -1,
-  duration: 1,
-  ease: "sine.inOut"
-});
-gsap.to("#planet", {
-  rotation: -2,
-  yoyo: true,
-  repeat: -1,
-  duration: 1,
-  ease: "sine.inOut",
-  transformOrigin: "50% 50%"
+anime({
+  targets: ".row svg",
+  translateY: 10,
+  autoplay: true,
+  loop: true,
+  easing: "easeInOutSine",
+  direction: "alternate"
 });
 
-gsap.to("#starsBig g", {
-  rotation: "random(-30,30)",
-  transformOrigin: "50% 50%",
-  yoyo: true,
-  repeat: -1,
-  ease: "sine.inOut"
+anime({
+  targets: "#zero",
+  translateX: 10,
+  autoplay: true,
+  loop: true,
+  easing: "easeInOutSine",
+  direction: "alternate",
+  scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+  rotateY: { value: "+=180", delay: 200 }
 });
-gsap.fromTo(
-  "#starsSmall g",
-  { scale: 0, transformOrigin: "50% 50%" },
-  { scale: 1, transformOrigin: "50% 50%", yoyo: true, repeat: -1, stagger: 0.1 }
-);
-gsap.to("#circlesSmall circle", {
-  y: -4,
-  yoyo: true,
-  duration: 1,
-  ease: "sine.inOut",
-  repeat: -1
-});
-gsap.to("#circlesBig circle", {
-  y: -2,
-  yoyo: true,
-  duration: 1,
-  ease: "sine.inOut",
-  repeat: -1
-});
-
-gsap.set("#glassShine", { x: -68 });
-
-gsap.to("#glassShine", {
-  x: 80,
-  duration: 2,
-  rotation: -30,
-  ease: "expo.inOut",
-  transformOrigin: "50% 50%",
-  repeat: -1,
-  repeatDelay: 8,
-  delay: 2
-});
-
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
-
-burger.addEventListener('click',(e) => {
-  burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed"
-  nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed"
-})
