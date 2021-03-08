ae0e2c0ba878c4ca6b9f15ae78aafa0c7b407494
opalhair <opalhair@gmail.com>
2021-03-08 17:03:35 +0800

    Site updated: 2021-03-08 17:03:34

--- 404/404.js
+++ 404/404.js
@@ -1,19 +1,77 @@
-anime({
-  targets: ".row svg",
-  translateY: 10,
-  autoplay: true,
-  loop: true,
-  easing: "easeInOutSine",
-  direction: "alternate"
-});
+function drawVisor() {
+  const canvas = document.getElementById('visor');
+  const ctx = canvas.getContext('2d');
+  
+  ctx.beginPath();
+  ctx.moveTo(5, 45);
+  ctx.bezierCurveTo(15, 64, 45, 64, 55, 45);
+  
+  ctx.lineTo(55, 20);
+  ctx.bezierCurveTo(55, 15, 50, 10, 45, 10);
+  
+  ctx.lineTo(15, 10);
+  
+  ctx.bezierCurveTo(15, 10, 5, 10, 5, 20);
+  ctx.lineTo(5, 45);
+  
+  ctx.fillStyle = '#2f3640';
+  ctx.strokeStyle = '#f5f6fa';
+  ctx.fill();
+  ctx.stroke();
+}
 
-anime({
-  targets: "#zero",
-  translateX: 10,
-  autoplay: true,
-  loop: true,
-  easing: "easeInOutSine",
-  direction: "alternate",
-  scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
-  rotateY: { value: "+=180", delay: 200 }
-});
+const cordCanvas = document.getElementById('cord');
+const ctx = cordCanvas.getContext('2d');
+
+let y1 = 160;
+let y2 = 100;
+let y3 = 100;
+
+let y1Forward = true;
+let y2Forward = false;
+let y3Forward = true;
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  
+  ctx.beginPath();
+  ctx.moveTo(130, 170);
+  ctx.bezierCurveTo(250, y1, 345, y2, 400, y3);
+  
+  ctx.strokeStyle = 'white';
+  ctx.lineWidth = 8;
+  ctx.stroke();
+ 
+  
+  if (y1 === 100) {
+    y1Forward = true;
+  }
+  
+  if (y1 === 300) {
+    y1Forward = false;
+  }
+  
+  if (y2 === 100) {
+    y2Forward = true;
+  }
+  
+  if (y2 === 310) {
+    y2Forward = false;
+  }
+  
+  if (y3 === 100) {
+    y3Forward = true;
+  }
+  
+  if (y3 === 317) {
+    y3Forward = false;
+  }
+  
+  y1Forward ? y1 += 1 : y1 -= 1;
+  y2Forward ? y2 += 1 : y2 -= 1;
+  y3Forward ? y3 += 1 : y3 -= 1;
+}
+
+drawVisor();
+animate();
