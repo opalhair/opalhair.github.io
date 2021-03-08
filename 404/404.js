6f98ec1b2e16034bf29e8ba253958e518a778fae
opalhair <opalhair@gmail.com>
2021-03-08 15:22:39 +0800

    Site updated: 2021-03-08 15:22:39

--- 404/404.js
+++ 404/404.js
@@ -1,87 +1,74 @@
-gsap.set("svg", { visibility: "visible" });
-gsap.to("#headStripe", {
-  y: 0.5,
-  rotation: 1,
-  yoyo: true,
-  repeat: -1,
-  ease: "sine.inOut",
-  duration: 1
-});
-gsap.to("#spaceman", {
-  y: 0.5,
-  rotation: 1,
-  yoyo: true,
-  repeat: -1,
-  ease: "sine.inOut",
-  duration: 1
-});
-gsap.to("#craterSmall", {
-  x: -3,
-  yoyo: true,
-  repeat: -1,
-  duration: 1,
-  ease: "sine.inOut"
-});
-gsap.to("#craterBig", {
-  x: 3,
-  yoyo: true,
-  repeat: -1,
-  duration: 1,
-  ease: "sine.inOut"
-});
-gsap.to("#planet", {
-  rotation: -2,
-  yoyo: true,
-  repeat: -1,
-  duration: 1,
-  ease: "sine.inOut",
-  transformOrigin: "50% 50%"
+// this following plugin is from http://bit.ly/2MSsHjC , I used it to make the eyes follow mouse direction.
+// used lil inspiration from: http://bit.ly/2M799CT
+//
+
+$(function () {
+  $.mousedirection();
+  $("body").bind("mousedirection", function (e) {
+  });
 });
 
-gsap.to("#starsBig g", {
-  rotation: "random(-30,30)",
-  transformOrigin: "50% 50%",
-  yoyo: true,
-  repeat: -1,
-  ease: "sine.inOut"
-});
-gsap.fromTo(
-  "#starsSmall g",
-  { scale: 0, transformOrigin: "50% 50%" },
-  { scale: 1, transformOrigin: "50% 50%", yoyo: true, repeat: -1, stagger: 0.1 }
-);
-gsap.to("#circlesSmall circle", {
-  y: -4,
-  yoyo: true,
-  duration: 1,
-  ease: "sine.inOut",
-  repeat: -1
-});
-gsap.to("#circlesBig circle", {
-  y: -2,
-  yoyo: true,
-  duration: 1,
-  ease: "sine.inOut",
-  repeat: -1
-});
 
-gsap.set("#glassShine", { x: -68 });
 
-gsap.to("#glassShine", {
-  x: 80,
-  duration: 2,
-  rotation: -30,
-  ease: "expo.inOut",
-  transformOrigin: "50% 50%",
-  repeat: -1,
-  repeatDelay: 8,
-  delay: 2
-});
 
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav');
+(function ($) {
+  var options = {};
+  var oldx = 0;
+  var oldy = 0;
+  var direction="";
+  $.mousedirection = function (opts) {
+    var defaults = {
+    };
+    options = $.extend(defaults, opts);
+    $(document).bind("mousemove", function (e) {
+      var activeElement = e.target || e.srcElement;
+      if (e.pageX > oldx && e.pageY > oldy) {
+        $('.RightEye').attr("class", "RightEyeRight");
+        $('.LeftEyeLeft').attr("class", "LeftEye");
+        direction="bottom-right";
 
-burger.addEventListener('click',(e) => {
-  burger.dataset.state === 'closed' ? burger.dataset.state = "open" : burger.dataset.state = "closed"
-  nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed"
-})
+      }
+      else if (e.pageX > oldx && e.pageY < oldy) {
+        $('.RightEye').attr("class", "RightEyeRight");
+        $('.LeftEyeLeft').attr("class", "LeftEye");
+
+        direction="top-right";
+      }
+
+      else if (e.pageX < oldx && e.pageY < oldy) {
+        $('.RightEyeRight').attr("class", "RightEye");
+        $('.LeftEye').attr("class", "LeftEyeLeft");
+
+        direction="top-left";
+      }
+      else if (e.pageX < oldx && e.pageY > oldy) {
+        $('.RightEyeRight').attr("class", "RightEye");
+        $('.LeftEye').attr("class", "LeftEyeLeft");
+
+        direction="bottom-left";
+      }
+      else if (e.pageX > oldx && e.pageY == oldy) {
+        $('.RightEye').attr("class", "RightEyeRight");
+        $('.LeftEyeLeft').attr("class", "LeftEye");
+
+        direction="right";
+      }
+      else if (e.pageX == oldx && e.pageY > oldy) {
+        direction="down";
+      }
+      else if (e.pageX == oldx && e.pageY < oldy) {
+        direction="up";
+      }
+      else if (e.pageX < oldx && e.pageY == oldy) {
+        $('.RightEyeRight').attr("class", "RightEye");
+        $('.LeftEye').attr("class", "LeftEyeLeft");
+
+        direction="left";
+      }
+      $(activeElement).trigger(direction);
+      $(activeElement).trigger({type:"mousedirection",direction:direction});
+      oldx=e.pageX;
+      oldy=e.pageY;
+    });
+  }
+})(jQuery);
